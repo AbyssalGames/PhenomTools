e3f52951c006bb7ff1011d83e2a495b2b43cd3a4
Phenomnom <stleavengood@gmail.com>
2021-01-18 21:56:13 -0800

    Added ReferenceFitter

---
+++ Assets/PhenomTools/UI/Editor/ReferenceFitterEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PhenomTools;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace PhenomTools
+{
+    [CustomEditor(typeof(ReferenceFitter), true)]
+    [CanEditMultipleObjects]
+    public class ReferenceFitterEditor : ContentSizeFitterEditor
+    {
+        private SerializedProperty refProperty;
+        private SerializedProperty layoutProperty;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            refProperty = serializedObject.FindProperty("reference");
+            layoutProperty = serializedObject.FindProperty("layout");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(refProperty);
+            EditorGUILayout.PropertyField(layoutProperty);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/PhenomTools/UI/ReferenceFitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PhenomTools;
+using UnityEngine.UI;
+
+namespace PhenomTools
+{
+    public class ReferenceFitter : ContentSizeFitter
+    {
+        [SerializeField]
+        private RectTransform reference = null;
+        [SerializeField]
+        private LayoutElement layout = null;
+
+        private void Update()
+        {
+            if (reference == null || layout == null)
+                return;
+
+            if (verticalFit == FitMode.PreferredSize)
+                layout.preferredHeight = reference.rect.height;
+        }
+    }
+}
