3ea6ee5365c1d989b72b6aa9c61f38e9d9e3aa3c
Phenomnom <stleavengood@gmail.com>
2022-01-25 16:33:14 -0800

    Added vector graphics package

---
+++ Assets/PhenomTools/Extensions/Singleton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace PhenomTools
+{
+    /// <summary>
+    /// Inherit from this base class to create a singleton.
+    /// e.g. public class MyClassName : Singleton<MyClassName> {}
+    /// </summary>
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    T[] objs = FindObjectsOfType(typeof(T)) as T[];
+
+                    if (objs.Length > 0)
+                        _instance = objs[0];
+                    else if (objs.Length > 1)
+                        Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                    else if(_instance == null)
+                        Debug.LogError("No instance of " + typeof(T).Name + " in the scene.");
+                }
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+    }
+}
