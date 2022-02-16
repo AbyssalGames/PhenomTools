d0f6be3d83566a9af1918cc9e0a0e397ed453b9b
Phenomnom <abyssaldevs@gmail.com>
2022-02-15 23:17:23 -0800

    Moved audio out of main package

--- EditorTools/SerializableDictionary/Editor/Definer.cs
+++ EditorTools/SerializableDictionary/Editor/Definer.cs
@@ -1,55 +1,55 @@
-using System.Collections.Generic;
-using UnityEditor;
+//using System.Collections.Generic;
+//using UnityEditor;
 
-namespace PhenomTools
-{
-    //[InitializeOnLoad]
-    public class Definer
-    {
-        static Definer()
-        {
-            List<string> defines = new List<string>(1)
-            {
-                "RHSD"
-            };
-            ApplyDefines(defines);
-        }
+//namespace PhenomTools
+//{
+//    //[InitializeOnLoad]
+//    public class Definer
+//    {
+//        static Definer()
+//        {
+//            List<string> defines = new List<string>(1)
+//            {
+//                "RHSD"
+//            };
+//            ApplyDefines(defines);
+//        }
 
-        public static void ApplyDefines(List<string> defines)
-        {
-            if (defines == null || defines.Count == 0)
-                return;
+//        public static void ApplyDefines(List<string> defines)
+//        {
+//            if (defines == null || defines.Count == 0)
+//                return;
 
-            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
+//            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+//            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
 
-            foreach (var define in defines)
-                if (!definesSplit.Contains(define))
-                    definesSplit.Add(define);
+//            foreach (var define in defines)
+//                if (!definesSplit.Contains(define))
+//                    definesSplit.Add(define);
 
-            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
-        }
+//            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
+//        }
 
-        public static void RemoveDefines(List<string> defines)
-        {
-            if (defines == null || defines.Count == 0)
-                return;
+//        public static void RemoveDefines(List<string> defines)
+//        {
+//            if (defines == null || defines.Count == 0)
+//                return;
 
-            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
+//            string availableDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+//            List<string> definesSplit = new List<string>(availableDefines.Split(';'));
 
-            foreach (var define in defines)
-                definesSplit.Remove(define);
+//            foreach (var define in defines)
+//                definesSplit.Remove(define);
 
-            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
-        }
+//            _ApplyDefine(string.Join(";", definesSplit.ToArray()));
+//        }
 
-        private static void _ApplyDefine(string define)
-        {
-            if (string.IsNullOrEmpty(define))
-                return;
+//        private static void _ApplyDefine(string define)
+//        {
+//            if (string.IsNullOrEmpty(define))
+//                return;
 
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, define);
-        }
-    }
-}
+//            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, define);
+//        }
+//    }
+//}
