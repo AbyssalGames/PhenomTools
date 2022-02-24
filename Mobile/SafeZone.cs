eb8d29a29b83f69a06a42b15951aa5b409c76384
Phenomnom <abyssaldevs@gmail.com>
2022-02-24 10:07:44 -0800

    Updated SafeZone

--- Mobile/SafeZone.cs
+++ Mobile/SafeZone.cs
@@ -1,60 +1,43 @@
 ﻿using UnityEngine;
 
-public class SafeZone : MonoBehaviour
+namespace PhenomTools
 {
-    //public static float trueWidth;
-    //public static float trueHeight;
-    public static float width;
-    public static float height;
-
-    RectTransform Panel;
-    //Rect LastSafeArea = new Rect(0, 0, 0, 0);
-
-    void Awake()
+    public class SafeZone : MonoBehaviour
     {
-        Panel = GetComponent<RectTransform>();
-        Refresh();
-    }
+        public static float width;
+        public static float height;
 
-    //void Update()
-    //{
-    //    Refresh();
-    //}
+        [SerializeField]
+        private RectTransform Panel;
 
-    void Refresh()
-    {
-        Rect safeArea = GetSafeArea();
+        private void Start()
+        {
+            Refresh();
+        }
 
-        //if (safeArea != LastSafeArea)
-            ApplySafeArea(safeArea);
-    }
+        public void Refresh()
+        {
+            ApplySafeArea(Screen.safeArea);
+        }
 
-    Rect GetSafeArea()
-    {
-        return Screen.safeArea;
-    }
+        private void ApplySafeArea(Rect r)
+        {
+            width = r.width;
+            height = r.height;
 
-    void ApplySafeArea(Rect r)
-    {
-        //LastSafeArea = r;
+            Vector2 anchorMin = r.position;
+            Vector2 anchorMax = r.position + r.size;
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+            Panel.anchorMin = anchorMin;
+            Panel.anchorMax = anchorMax;
+        }
 
-        width = r.width;
-        height = r.height;
-
-        // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
-        Vector2 anchorMin = r.position;
-        Vector2 anchorMax = r.position + r.size;
-        anchorMin.x /= Screen.width;
-        anchorMin.y /= Screen.height;
-        anchorMax.x /= Screen.width;
-        anchorMax.y /= Screen.height;
-        Panel.anchorMin = anchorMin;
-        Panel.anchorMax = anchorMax;
-
-        //width = Panel.sizeDelta.x;
-        //height = Panel.sizeDelta.y;
-
-        //Debug.LogFormat("New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
-        //    name, r.x, r.y, r.width, r.height, Screen.width, Screen.height);
+        private void Reset()
+        {
+            Panel = GetComponent<RectTransform>();
+        }
     }
 }
